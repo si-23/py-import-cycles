3d3211dc481cad1d377b9f76ee6cb302edeba329
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2022-03-28 16:25:03 +0200

    Type Tarjan implementation

--- project/tarjan.py
+++ project/tarjan.py
@@ -1,4 +1,9 @@
-def strongly_connected_components(graph):
+from typing import Hashable, List, Mapping, MutableMapping, Sequence, Tuple, TypeVar
+
+T = TypeVar("T", bound=Hashable)
+
+
+def strongly_connected_components(graph: Mapping[T, Sequence[T]]) -> Sequence[Tuple[T, ...]]:
     """
     Tarjan's Algorithm (named for its discoverer, Robert Tarjan) is a graph theory algorithm
     for finding the strongly connected components of a graph.
@@ -6,13 +11,13 @@ def strongly_connected_components(graph):
     Based on: http://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
     """
 
-    index_counter = [0]
-    stack = []
-    lowlinks = {}
-    index = {}
-    result = []
+    index_counter: List[int] = [0]
+    stack: List[T] = []
+    lowlinks: MutableMapping[T, int] = {}
+    index: MutableMapping[T, int] = {}
+    result: List[Tuple[T, ...]] = []
 
-    def strongconnect(node):
+    def strongconnect(node: T) -> None:
         # set the depth index for this node to the smallest unused index
         index[node] = index_counter[0]
         lowlinks[node] = index_counter[0]
@@ -22,7 +27,7 @@ def strongly_connected_components(graph):
         # Consider successors of `node`
         try:
             successors = graph[node]
-        except:
+        except KeyError:
             successors = []
         for successor in successors:
             if successor not in lowlinks:
@@ -30,7 +35,8 @@ def strongly_connected_components(graph):
                 strongconnect(successor)
                 lowlinks[node] = min(lowlinks[node], lowlinks[successor])
             elif successor in stack:
-                # the successor is in the stack and hence in the current strongly connected component (SCC)
+                # the successor is in the stack and hence in the current
+                # strongly connected component (SCC)
                 lowlinks[node] = min(lowlinks[node], index[successor])
 
         # If `node` is a root node, pop the stack and generate an SCC
