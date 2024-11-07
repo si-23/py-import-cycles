f2d9e99bab55a3da1027b9ec40d31c56219627ae
Simon Jess <simon.jess@checkmk.com>
2024-11-07 08:54:42 +0100

    Fix ignoring paths

--- py_import_cycles/files.py
+++ py_import_cycles/files.py
@@ -11,12 +11,17 @@ from .modules import PyModule
 
 
 def scan_packages(packages: Sequence[Path]) -> Iterator[PyModule]:
+    paths_to_ignore: set[Path] = set()
     for package_path in packages:
         for root, _dirs, files in os.walk(package_path):
             root_path = Path(root)
 
-            if root_path.name.startswith("."):
-                break
+            if root_path.name.startswith(".") or root_path.name == "__pycache_":
+                paths_to_ignore.add(root_path)
+                continue
+
+            if any(root_path.is_relative_to(p) for p in paths_to_ignore):
+                continue
 
             for file in files:
                 # Regular package or Python module
