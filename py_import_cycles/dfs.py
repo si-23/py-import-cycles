66dc6a4bba2113c92e75d336162bd9aeb90c199b
Simon Jess <simon.jess@checkmk.com>
2026-04-07 15:45:36 +0200

    dfs: Move pseudo local func

--- py_import_cycles/dfs.py
+++ py_import_cycles/dfs.py
@@ -10,19 +10,20 @@ from .type_defs import Comparable
 T = TypeVar("T", bound=Comparable)
 
 
+def _make_cycle(cyclic_edges: Sequence[tuple[T, T, str]]) -> tuple[T, ...]:
+    # find_cycle returns:
+    # [
+    #     (e0, e1, ORIENTATION),
+    #     (e1, e2, ORIENTATION),
+    #     ...
+    #     (eX-1, eX, ORIENTATION),
+    #     (eX, e0, ORIENTATION),
+    # ]
+    return tuple(cyclic_edges[0][:-1] + tuple(ce[1] for ce in cyclic_edges[1:-1]))
+
+
 def dfs(graph: Mapping[T, Sequence[T]]) -> Iterator[tuple[T, ...]]:
     known_cycles: Set[tuple[T, ...]] = set()
-
-    def _make_cycle(cyclic_edges: Sequence[tuple[T, T, str]]) -> tuple[T, ...]:
-        # find_cycle returns:
-        # [
-        #     (e0, e1, ORIENTATION),
-        #     (e1, e2, ORIENTATION),
-        #     ...
-        #     (eX-1, eX, ORIENTATION),
-        #     (eX, e0, ORIENTATION),
-        # ]
-        return tuple(cyclic_edges[0][:-1] + tuple(ce[1] for ce in cyclic_edges[1:-1]))
 
     G = DiGraph([(v, w) for v, vertices in graph.items() for w in vertices])
     for vertex in sorted(graph):
