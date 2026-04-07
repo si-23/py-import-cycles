36e013233bf75c856cac074f099b04694464a50d
Simon Jess <simon.jess@checkmk.com>
2026-04-07 15:43:27 +0200

    tarjan: Use strongly_connected_components from networkx

--- py_import_cycles/tarjan.py
+++ py_import_cycles/tarjan.py
@@ -1,64 +1,16 @@
 #!/usr/bin/env python3
 
-from collections.abc import Hashable, Mapping, Sequence
-from typing import MutableMapping, Tuple, TypeVar
+from collections.abc import Hashable, Iterator, Mapping, Sequence
+from typing import Tuple, TypeVar
+
+from networkx import DiGraph, strongly_connected_components
 
 T = TypeVar("T", bound=Hashable)
 
 
-def strongly_connected_components(
-    graph: Mapping[T, Sequence[T]],
-) -> Sequence[Tuple[T, ...]]:
-    """
-    Tarjan's Algorithm (named for its discoverer, Robert Tarjan) is a graph theory algorithm
-    for finding the strongly connected components of a graph.
-
-    Based on: http://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
-    """
-
-    index_counter: list[int] = [0]
-    stack: list[T] = []
-    lowlinks: MutableMapping[T, int] = {}
-    index: MutableMapping[T, int] = {}
-    result: list[Tuple[T, ...]] = []
-
-    def strongconnect(node: T) -> None:
-        # set the depth index for this node to the smallest unused index
-        index[node] = index_counter[0]
-        lowlinks[node] = index_counter[0]
-        index_counter[0] += 1
-        stack.append(node)
-
-        # Consider successors of `node`
-        try:
-            successors = graph[node]
-        except KeyError:
-            successors = []
-        for successor in successors:
-            if successor not in lowlinks:
-                # Successor has not yet been visited; recurse on it
-                strongconnect(successor)
-                lowlinks[node] = min(lowlinks[node], lowlinks[successor])
-            elif successor in stack:
-                # the successor is in the stack and hence in the current
-                # strongly connected component (SCC)
-                lowlinks[node] = min(lowlinks[node], index[successor])
-
-        # If `node` is a root node, pop the stack and generate an SCC
-        if lowlinks[node] == index[node]:
-            connected_component = []
-
-            while True:
-                successor = stack.pop()
-                connected_component.append(successor)
-                if successor == node:
-                    break
-            component = tuple(connected_component)
-            # storing the result
-            result.append(component)
-
-    for node in graph:
-        if node not in lowlinks:
-            strongconnect(node)
-
-    return result
+def tarjan(graph: Mapping[T, Sequence[T]]) -> Iterator[Tuple[T, ...]]:
+    for scc in strongly_connected_components(
+        DiGraph([(v, w) for v, vertices in graph.items() for w in vertices])
+    ):
+        if len(scc) > 1:
+            yield tuple(scc)
